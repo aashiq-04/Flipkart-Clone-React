fc5826287233e34118acc32de13963ad23893f4b
aashiq-04 <mohd04aashiq@gmail.com>
2023-05-29 12:41:14 +0530

    slider

--- src/App.js
+++ src/App.js
@@ -4,25 +4,19 @@ import Slide from './slide';
 import Top from './top';
 
 function App() {
-  const slides = [
-    { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/645cf9502f202096.jpg?q=50" },
-    { url: "https://rukminim1.flixcart.com/fk-p-flap/844/140/image/d6dc40011f48d2da.jpg?q=50" },
-    { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/9ec62d3c41932728.jpg?q=50" },
-    { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/30b0e892420793da.jpg?q=50" }
-  ]
-  const containerStyles={
-    width: "500px",
-    height: ""
-  }
+  // const slides = [
+  //   { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699" },
+  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/844/140/image/d6dc40011f48d2da.jpg?q=50" },
+  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/9ec62d3c41932728.jpg?q=50" },
+  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/30b0e892420793da.jpg?q=50" }
+  // ]
 
 
   return (
     <div className="App">
       <Navbar />
       <Top />
-      <div style={containerStyles}>
-        <Slide slides={slides} />
-      </div>
+      <Slide/>
     </div>
   );
 }
--- src/slide.js
+++ src/slide.js
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
-const Slide = ({ slides }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
 
+const Slide = () => {
     return (
-        <div className="slides">
-            <div style={{ backgroundImage: `url(${slides[currentIndex]})` }}>
-                {/* <img src="https://rukminim1.flixcart.com/fk-p-flap/50/50/image/645cf9502f202096.jpg?q=50" alt="aaa" /> */}
+        <div className="slides" style={{ height: "280px" }}>
+            <div className="left">
+                <svg width="14.6" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="l-greater"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="FXox6K"></path></svg>
             </div>
+            <div className="slide">
+                <a href="lll">
+                    <img className="img1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699" alt="sample" />
+
+                </a>
+            </div>
+            <div className="right">
+                <svg width="14.6" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="r-greater"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="FXox6K"></path></svg>
+            </div>
+
         </div>
     );
 }
