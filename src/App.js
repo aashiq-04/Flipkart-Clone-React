58c648881f3c2469d5a0983c9dc2c6e10976c9e9
aashiq-04 <mohd04aashiq@gmail.com>
2023-06-26 12:53:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import Slide from './slide';
 import Top from './top';
 
 function App() {
-  // const slides = [
-  //   { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699" },
-  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/844/140/image/d6dc40011f48d2da.jpg?q=50" },
-  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/9ec62d3c41932728.jpg?q=50" },
-  //   { url: "https://rukminim1.flixcart.com/fk-p-flap/50/50/image/30b0e892420793da.jpg?q=50" }
-  // ]
-
 
   return (
     <div className="App">
       <Navbar />
       <Top />
-      <Slide/>
+      <Slide />
+      <div className='sample'>
+
+      </div>
     </div>
   );
 }
--- src/slide.js
+++ src/slide.js
@@ -2,22 +2,30 @@ import { useState } from "react";
 
 
 const Slide = () => {
+    const [Image, setImage] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699");
+
+
+    const change=()=>{
+        setImage("https://rukminim1.flixcart.com/fk-p-flap/844/140/image/d6dc40011f48d2da.jpg?q=50");
+    }
+
+    const back=()=>{
+        setImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699");
+    }
     return (
         <div className="slides" style={{ height: "280px" }}>
-            <div className="left">
-                <svg width="14.6" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="l-greater"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="FXox6K"></path></svg>
-            </div>
+
+            <button className="left-button" onClick={back}>&lt;</button>
             <div className="slide">
                 <a href="lll">
-                    <img className="img1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXZ8v2NPgYQAnfLQFkS84FugqdsxqEyxhJfVttwlRZCQ&usqp=CAU&ec=48665699" alt="sample" />
+                    <img className="img1" src={Image} alt="sample" />
 
                 </a>
             </div>
-            <div className="right">
-                <svg width="14.6" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" class="r-greater"><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#fff" class="FXox6K"></path></svg>
-            </div>
+            <button className="right-button" onClick={change}> &gt;</button>
 
         </div>
+
     );
 }
 
