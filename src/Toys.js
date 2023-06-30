ff6027318175a9f933d3bed1e6ac9f5f951eb07d
aashiq-04 <mohd04aashiq@gmail.com>
2023-06-30 15:35:09 +0530

    Toys Section added

--- src/Toys.js
+++ src/Toys.js
@@ -1,16 +1,70 @@
 const Toys = () => {
-    return ( 
+    return (
         <div className="toys">
             <div className="toy-top">
                 <div className="TT-head">
-                Beauty,Food, Toys & more
+                    Beauty,Food, Toys & more
                 </div>
             </div>
             <div className="itemsT">
-
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/400/400/kdbzqfk0/bar/8/h/q/push-up-bar-0-8-long-ankaro-original-imafu9dmvdk3rzvy.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Gym Essentials </div>
+                        <div className="Tdiv2">From 139$ </div>
+                    </a>
+                </div>
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/612/612/xif0q/remote-control-toy/f/h/u/remote-control-toy-car-electric-chargeable-3d-lightning-black-1-original-imagz45fsehbh2fh.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Remote Control Toys </div>
+                        <div className="Tdiv2">Up to 80% Off </div>
+                    </a>
+                </div>
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/612/612/khmbafk0-0/helmet/a/4/i/sbh-17-terminator-full-face-graphic-helmet-in-matt-fluo-blue-original-imafxhe8wesqzfrj.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Rider Helmuts </div>
+                        <div className="Tdiv2">From 699$ </div>
+                    </a>
+                </div>
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/612/612/xif0q/cycle/6/x/h/-original-imagpa5rygjfwwjs.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Non-Geared Cycles </div>
+                        <div className="Tdiv2">Up to 40% Off </div>
+                    </a>
+                </div>
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/612/612/xif0q/paper/h/h/2/-original-imagqte9dy3wuzwm.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Top Selling Stationary </div>
+                        <div className="Tdiv2">From 49$ </div>
+                    </a>
+                </div>
+                <div className="Titems">
+                    <a href="#" className="Tanch">
+                        <div className="timg">
+                            <img src="https://rukminim1.flixcart.com/image/400/400/xif0q/musical-toy/b/v/u/kids-37-key-piano-keyboard-with-recording-mic-mobile-charger-original-imagh4hcfz3ghdbd.jpeg?q=70" alt="Toyimg" />
+                        </div>
+                        <div className="Tdiv1"> Musical Keyboards </div>
+                        <div className="Tdiv2">Up to 70% Off </div>
+                    </a>
+                </div>
+                <button className="Tright-button"> &gt;</button>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Toys;
